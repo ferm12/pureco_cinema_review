89193a5290053fadd651f5ae9773573e5712bb7f
Fermin Pureco <ferminpureco@Fermin-Purecos-MacBook-Pro.local>
2017-04-23 14:54:10 -0700

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root "movies#index"
+    get "movies"        => "movies#index"
+    get "movies/:id"    => "movies#show", as: "movie"
+    get "events/:id/edit" => "events#edit", as: "edit_event"
+end

43ae5efa6305bc5e39ebb14694432227a6d34bf4
Fermin Pureco <ferminpureco@Fermin-Purecos-MacBook-Pro.local>
2017-04-23 16:25:11 -0700

    Added edit page to edit each movie individualy and wrote test case

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root "movies#index"
-    get "movies"        => "movies#index"
-    get "movies/:id"    => "movies#show", as: "movie"
-    get "movie/:id/edit" => "movies#edit", as: "edit_movie"
+    root    "movies#index"
+    get     "movies"            => "movies#index"
+    get     "movies/:id"        => "movies#show", as: "movie"
+    get     "movie/:id/edit"    => "movies#edit", as: "edit_movie"
+    patch   "movies/:id"         => "movies#update" 
 end

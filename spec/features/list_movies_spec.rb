8c0b24defadcf90f5f0524d18f9152e37dacb627
Fermin Pureco <ferminpureco@Fermin-Purecos-MacBook-Pro.local>
2017-04-23 19:42:45 -0700

    Added a query to order movies by release_date asc and dec order, also added some test cases

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -35,5 +35,33 @@ describe "Viewing the list of movies" do
         expect(page).to have_text("$318,412,101.00")
         
   end
+
+    it "does not show a movie that hasn't yet been released" do
+        movie = Movie.create(movie_attributes(released_on: 1.month.from_now))
+
+        visit movies_path
+
+        expect(page).not_to have_text(movie.title)
+    end
+
+    it "is released when the released on date is in the past" do
+        movie = Movie.create(movie_attributes(released_on: 3.months.ago))
+
+        expect(Movie.released).to include(movie)
+    end
+
+    it "is not released when the released on date is in the future" do
+        movie = Movie.create(movie_attributes(released_on: 3.months.from_now))
+
+        expect(Movie.released).not_to include(movie)
+    end
+
+    it "returns released movies ordered with the most recently-released movie first" do
+        movie1 = Movie.create(movie_attributes(released_on: 3.months.ago))
+        movie2 = Movie.create(movie_attributes(released_on: 2.months.ago))
+        movie3 = Movie.create(movie_attributes(released_on: 1.months.ago))
+
+        expect(Movie.released).to eq([movie3, movie2, movie1])
+    end
   
 end

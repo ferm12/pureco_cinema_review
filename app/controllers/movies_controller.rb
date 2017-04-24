9740e8f0e1c4cbd69fa5fb32f38b297cee4d9dd8
Fermin Pureco <ferminpureco@Fermin-Purecos-MacBook-Pro.local>
2017-04-24 05:22:34 -0700

    Adding some style and images to the pureco_cinema_review app and

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,6 +37,6 @@ class MoviesController < ApplicationController
     private
 
     def movie_params
-        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :image_file_name)
     end
 end
